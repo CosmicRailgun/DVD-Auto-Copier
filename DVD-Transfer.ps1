407b0845d8caa7ac367f372b8f97feb4b47a436c
CosmicRailgun <35468063+CosmicRailgun@users.noreply.github.com>
2021-07-06 15:03:25 -0500

    Update DVD-Transfer.ps1
    Fixed movie file size detection, added finish sound, cleaned up file paths

--- DVD-Transfer.ps1
+++ DVD-Transfer.ps1
@@ -1,5 +1,20 @@
-﻿#Check if DVD loaded
+#Check if DVD loaded
 $dvdLoaded = (Get-WMIObject -Class Win32_CDROMDrive -Property *).MediaLoaded
+
+#DVD complete sound
+Function DVD-Done {
+	[System.Console]::Beep(659, 100);
+	[System.Console]::Beep(659, 100);
+	[System.Threading.Thread]::Sleep(125);
+	[System.Console]::Beep(659, 100);
+	[System.Threading.Thread]::Sleep(167);
+	[System.Console]::Beep(523, 100);
+	[System.Console]::Beep(659, 110);
+	[System.Threading.Thread]::Sleep(125);
+	[System.Console]::Beep(784, 125);
+	[System.Threading.Thread]::Sleep(375);
+	[System.Console]::Beep(392, 125);
+}
 
 
 
@@ -11,22 +26,23 @@ if ($dvdLoaded)
     $DVDDrive = (Get-WMIObject -Class Win32_CDROMDrive -Property *).Drive
     
     #Handbrake Preset Setup
-    .\HandBrakeCLI --preset-import-file <#Path to preset.json#> -Z "DVD"
+    .\HandBrakeCLI --preset-import-file C:\Users\admin\Desktop\HandBrakeCLI-1.3.3-win-x86_64\presets\DVD.json -Z "DVD"
 
     #Get DVD Name
     $TitleName = (Get-WMIObject -Class Win32_CDROMDrive -Property *).VolumeName
 
-    #Set Directory
-    $Dir = "C:\$TitleName\"
+    #Set and Make Directory
+    New-Item -Path "C:\Users\admin\Desktop\DVD-RIP\" -Name "$TitleName" -ItemType "directory"
+    $Dir = "C:\Users\admin\Desktop\DVD-RIP\$TitleName\"
 
     for ($title = 1 ; $title -le 30 ; $title++){
         
         #Encode
-        .\HandBrakeCLI --preset-import-file <#Path to preset.json#> -Z "DVD" -t $title -scan -min-duration 60 -i $DVDDrive\ -o "C:\$TitleName\$TitleName-$Title.mp4"
+        .\HandBrakeCLI --preset-import-file C:\Users\admin\Desktop\HandBrakeCLI-1.3.3-win-x86_64\presets\DVD.json -Z "DVD" -t $title -scan -min-duration 60 -i $DVDDrive\ -o "$Dir$TitleName-$Title.mp4"
         
         #Checks to see if the file is large, if it is, the system will assume it's a movie and annihilate all files smaller than it
-        $FileSize = ((Get-Item "C:\$TitleName\$TitleName-$Title.mp4").length/1MB)
-        if ( $FileSize > 600 ){
+        $FileSize = ((Get-Item "$Dir$TitleName-$Title.mp4").length/1MB)
+        if ( $FileSize -gt 600 ){
         $Movie = $true
         }
     }
@@ -35,6 +51,7 @@ if ($dvdLoaded)
 
     #Eject Disk
     start powershell .\eject.ps1
+    DVD-Done
 
     #Check if movie or not
 
@@ -45,7 +62,7 @@ if ($dvdLoaded)
 
     }else{
         #Delete Tiny Titles/Transfers
-        $SizeMin = 40 #MB
+        $SizeMin = 50 #MB
         Get-ChildItem -Path $Dir -Recurse | Where { $_.Length / 1MB -lt $SizeMin } | Remove-Item -Force
     }
 
