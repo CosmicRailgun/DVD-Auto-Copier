2722ee67d3d59cfe08c016b400cbaa35dc9d040f
CosmicRailgun <35468063+CosmicRailgun@users.noreply.github.com>
2021-07-07 09:33:31 -0500

    Update DVD-Transfer.ps1
    Adjusted movie file size thresholds, added variables for handbrake preset to make it easier to switch between presets, adjusted script loop system so it's actually a loop rather than 2 script files executing each other....heh

--- DVD-Transfer.ps1
+++ DVD-Transfer.ps1
@@ -1,10 +1,7 @@
-#Check if DVD loaded
-$dvdLoaded = (Get-WMIObject -Class Win32_CDROMDrive -Property *).MediaLoaded
-
 #DVD complete sound
 Function DVD-Done {
 	[System.Console]::Beep(659, 100);
-	[System.Console]::Beep(659, 100);
+	[System.Console]::Beep(659, 105);
 	[System.Threading.Thread]::Sleep(125);
 	[System.Console]::Beep(659, 100);
 	[System.Threading.Thread]::Sleep(167);
@@ -16,35 +13,53 @@ Function DVD-Done {
 	[System.Console]::Beep(392, 125);
 }
 
+#Set Preset
+$PresetID = "DVD"
+$PresetFile = "DVD.json"
+
+
+#Loop
+while ($true){
+
+
+#Check if DVD loaded
+$dvdLoaded = (Get-WMIObject -Class Win32_CDROMDrive -Property *).MediaLoaded
+
+
+
 
 
 if ($dvdLoaded)
 {
     Write-Output "Beginning DVD transfer"
 
-    #Set DVD drive letter
-    $DVDDrive = (Get-WMIObject -Class Win32_CDROMDrive -Property *).Drive
-    
-    #Handbrake Preset Setup
-    .\HandBrakeCLI --preset-import-file C:\Users\admin\Desktop\HandBrakeCLI-1.3.3-win-x86_64\presets\DVD.json -Z "DVD"
-
-    #Get DVD Name
-    $TitleName = (Get-WMIObject -Class Win32_CDROMDrive -Property *).VolumeName
-
     #Set and Make Directory
     New-Item -Path "C:\Users\admin\Desktop\DVD-RIP\" -Name "$TitleName" -ItemType "directory"
     $Dir = "C:\Users\admin\Desktop\DVD-RIP\$TitleName\"
 
+    #Set DVD drive letter
+    $DVDDrive = (Get-WMIObject -Class Win32_CDROMDrive -Property *).Drive
+    
+    #Handbrake Preset Setup
+    .\HandBrakeCLI --preset-import-file .\Presets\$PresetFile -Z "$PresetID"
+
+    #Get DVD Name
+    $TitleName = (Get-WMIObject -Class Win32_CDROMDrive -Property *).VolumeName
+
     for ($title = 1 ; $title -le 30 ; $title++){
         
         #Encode
-        .\HandBrakeCLI --preset-import-file C:\Users\admin\Desktop\HandBrakeCLI-1.3.3-win-x86_64\presets\DVD.json -Z "DVD" -t $title -scan -min-duration 60 -i $DVDDrive\ -o "$Dir$TitleName-$Title.mp4"
+        .\HandBrakeCLI --preset-import-file .\Presets\$PresetFile -Z "$PresetID" -t $title -scan -min-duration 60 -i $DVDDrive\ -o "$Dir$TitleName-$Title.mp4"
         
         #Checks to see if the file is large, if it is, the system will assume it's a movie and annihilate all files smaller than it
         $FileSize = ((Get-Item "$Dir$TitleName-$Title.mp4").length/1MB)
-        if ( $FileSize -gt 600 ){
-        $Movie = $true
-        }
+        if ( $FileSize -gt 1200 ){
+            $Movie = $true
+            }
+
+        if ($Movie){
+            $title = 31
+            }
     }
 
     Write-Output "Transfer Complete"
@@ -52,17 +67,19 @@ if ($dvdLoaded)
     #Eject Disk
     start powershell .\eject.ps1
     DVD-Done
+    #close other instance of powershell
+    Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
 
     #Check if movie or not
 
 
     if ( $Movie ) {
-        $SizeMin = 500 #MB
+        $SizeMin = 600 #MB
         Get-ChildItem -Path $Dir -Recurse | Where { $_.Length / 1MB -lt $SizeMin } | Remove-Item -Force
 
     }else{
         #Delete Tiny Titles/Transfers
-        $SizeMin = 50 #MB
+        $SizeMin = 100 #MB
         Get-ChildItem -Path $Dir -Recurse | Where { $_.Length / 1MB -lt $SizeMin } | Remove-Item -Force
     }
 
@@ -71,8 +88,10 @@ if ($dvdLoaded)
 
 }
 
-Start-Sleep -s 10
+Start-Sleep -s 2
 
-#close other instance of powershell
-Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
-.\restart.ps1
+#Write time/date
+Get-Date | Out-File -FilePath .\log.txt
+
+
+}
